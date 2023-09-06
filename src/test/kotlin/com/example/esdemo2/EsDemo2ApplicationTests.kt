1d63abc8fca02dcf02be7bb984324a8fa6868691
유지민 <86466319+jimin-yu@users.noreply.github.com>
2023-09-06 18:26:57 +0900

    feat: first commit

---
+++ src/test/kotlin/com/example/esdemo2/EsDemo2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.esdemo2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EsDemo2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
